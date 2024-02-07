8e215ef2632c01476e78866a695b48f6b3a27bdc
ghost <jhonsnow751@gmail.com>
2024-02-07 11:14:47 +0100

    Djhvdhubnadbgvghvh

--- src/config/firebaseinit.js
+++ src/config/firebaseinit.js
@@ -12,12 +12,12 @@ import { authState } from "rxfire/auth";
 
 // Set up Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAF41r0vB8WtTIgivFYG5MqrpSQ-4hyRUw",
-  authDomain: "bitmax-49d16.firebaseapp.com",
-  projectId: "bitmax-49d16",
-  storageBucket: "bitmax-49d16.appspot.com",
-  messagingSenderId: "262820151297",
-  appId: "1:262820151297:web:b91b45c8665a62a9e5c000"
+  apiKey: "AIzaSyBr3vAiUJURiddaM6xF0IGgkUsw3BsIfsg",
+  authDomain: "bitmax-937a0.firebaseapp.com",
+  projectId: "bitmax-937a0",
+  storageBucket: "bitmax-937a0.appspot.com",
+  messagingSenderId: "738537975374",
+  appId: "1:738537975374:web:b160efd459ffbb0bce1d1f"
 };
 
 
